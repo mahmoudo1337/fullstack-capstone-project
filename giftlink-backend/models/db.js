9f8964f16cb38653b3a53880ae245750a7bcc449
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-12-28 00:30:55 +0200

    initializing gift routes

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,6 +1,6 @@
 // db.js
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
 
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
@@ -9,20 +9,27 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+  if (dbInstance) {
+    return dbInstance;
+  }
 
-    const client = new MongoClient(url);      
+  const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+  // Task 1: Connect to MongoDB
+  try {
+    await client.connect();
+    console.log("connected to mongodb");
+    dbInstance = client.db(dbName);
+    return dbInstance;
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+  }
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+  // Task 2: Connect to database giftDB and store in variable dbInstance
+  //{{insert code}}
 
-    // Task 3: Return database instance
-    // {{insert code}}
+  // Task 3: Return database instance
+  // {{insert code}}
 }
 
 module.exports = connectToDatabase;
