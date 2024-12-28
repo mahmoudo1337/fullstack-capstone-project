3f12e6da99602a161e2d58442db7adf57e8c9274
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-12-28 03:39:19 +0200

    fix profile route

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,6 +4,7 @@ import MainPage from "./components/MainPage/MainPage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
 import DetailsPage from "./components/DetailsPage/DetailsPage";
+import Profile from "./components/Profile/Profile";
 import SearchPage from "./components/SearchPage/SearchPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -21,6 +22,7 @@ function App() {
         <Route path="/app/register" element={<RegisterPage />} />
         <Route path="/app/product/:productId" element={<DetailsPage />} />
         <Route path="/app/search" element={<SearchPage />} />
+        <Route path="/app/profile" element={<Profile />} />
       </Routes>
     </>
   );
